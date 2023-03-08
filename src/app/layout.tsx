cce0b57b0765b7eb43a565cfdcc04882512c4c17
Josue Ale <josue.ale@materiagris.pe>
2023-03-08 08:29:05 -0500

    fix: seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,59 @@
 import './globals.css';
+import type { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'ElDetalle - Empresa Electromecánica Especializada en Fibra Óptica',
   // <!-- SEO meta tags -->
   description:
     'Somos una empresa especializada en electromecánica para fibra óptica. Ofrecemos soluciones innovadoras y de calidad para tus proyectos.',
   keywords:
     'empresa, electromecánica, fibra óptica, soluciones, calidad, proyectos',
-  author: 'Tu Nombre Aquí',
-  robots: 'index,follow',
-  googlebot: 'index,follow',
-  bingbot: 'index,follow',
-  rating: 'General',
-  language: 'Spanish',
-  'revisit-after': '7 days',
+  authors: [{ name: 'Josue ale' }],
+
+  other: {
+    'revisit-after': '7 days',
+  },
+
   referrer: 'no-referrer-when-downgrade',
 
-  // <!-- Open Graph meta tags for social media -->
-  'og:title': 'Empresa Electromecánica Especializada en Fibra Óptica',
-  'og:description':
-    'Somos una empresa especializada en electromecánica para fibra óptica. Ofrecemos soluciones innovadoras y de calidad para tus proyectos.',
-  'og:url': 'http://www.tupagina.com',
-  'og:image': 'https://eldetalleperu.com/wp-content/uploads/2019/09/LOGO-REDIBUJADO-EL-DETALLE.png',
-  'og:type': 'website',
-  'og:locale': 'es_ES',
+  openGraph: {
+    title: 'Empresa Electromecánica Especializada en Fibra Óptica',
+    description:
+      'Somos una empresa especializada en electromecánica para fibra óptica. Ofrecemos soluciones innovadoras y de calidad para tus proyectos.',
+    url: '/',
+    siteName: 'el detalle',
+    images: [
+      {
+        url: '/logo-eldetalle.png',
+        width: 800,
+        height: 600,
+        alt: 'company logo',
+      },
+    ],
+    locale: 'es-ES',
+    type: 'website',
+  },
 
-  // <!-- Twitter Card meta tags -->
-  'twitter:card': 'summary_large_image',
-  'twitter:title': 'Empresa Electromecánica Especializada en Fibra Óptica',
-  'twitter:description':
-    'Somos una empresa especializada en electromecánica para fibra óptica. Ofrecemos soluciones innovadoras y de calidad para tus proyectos.',
-  'twitter:image': 'https://eldetalleperu.com/wp-content/uploads/2019/09/LOGO-REDIBUJADO-EL-DETALLE.png',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: '',
+    description: '',
+    images: '/logo-eldetalle.png',
+  },
 };
 
 export default function RootLayout({
